131905b212cbf8e9b3a265efbaf77670bcadb78e
bxviet515 <vietsdk6@gmail.com>
2025-05-16 22:29:49 +0700

    Flip the unit

--- Assets/hvo/Scripts/Units/Unit.cs
+++ Assets/hvo/Scripts/Units/Unit.cs
@@ -7,6 +7,7 @@ public abstract class Unit : MonoBehaviour
     public bool isMoving;
     protected Animator m_Animator;
     protected AIPawn m_AIPawn;
+    protected SpriteRenderer m_SpriteRenderer;
     protected void Awake()
     {
         if (TryGetComponent<Animator>(out var animator))
@@ -18,10 +19,13 @@ public abstract class Unit : MonoBehaviour
             m_AIPawn = aIPawn;
         }
 
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
 
     }
     public void MoveTo(Vector3 destination)
     {
+        var direction = (destination - transform.position).normalized;
+        m_SpriteRenderer.flipX = direction.x < 0;
         m_AIPawn.SetDestination(destination);
     }
 }
