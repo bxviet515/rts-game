507d47097db67fd68c136b64610da1f79e0b9a30
bxviet515 <vietsdk6@gmail.com>
2025-05-17 17:35:46 +0700

    Animation Curve for PointToClick

--- Assets/hvo/Scripts/UI/PointToClick.cs
+++ Assets/hvo/Scripts/UI/PointToClick.cs
@@ -4,7 +4,10 @@ public class PointToClick : MonoBehaviour
 {
     [SerializeField] private float m_Duration = 1f;
     [SerializeField] private SpriteRenderer m_SpriteRenderer;
+    [SerializeField] private AnimationCurve m_ScaleCurve;
     private float m_Timer;
+    private Vector3 m_InitialScale;
+    private float m_FreqTimer;
     private PointToClickPool m_Pool;
 
     public void Init(PointToClickPool pool)
@@ -15,9 +18,16 @@ public class PointToClick : MonoBehaviour
         gameObject.SetActive(true);
     }
 
+    private void Start() {
+        m_InitialScale = transform.localScale;
+    }
     private void Update()
     {
         m_Timer += Time.deltaTime;
+        m_FreqTimer += Time.deltaTime;
+        m_FreqTimer %= 1f;
+        float scaleMultiplier = m_ScaleCurve.Evaluate(m_FreqTimer);
+        transform.localScale = m_InitialScale * scaleMultiplier;
         if (m_Timer >= m_Duration * 0.9f)
         {
             float fadeProgress = (m_Timer - m_Duration * 0.9f) / (m_Duration * 0.1f);
