77768fc8f15af025317af95ce299f66b22f0f4bb
bxviet515 <vietsdk6@gmail.com>
2025-05-19 18:42:39 +0700

    Offset the position

--- Assets/hvo/Scripts/Units/Actions/BuildActionSO.cs
+++ Assets/hvo/Scripts/Units/Actions/BuildActionSO.cs
@@ -7,9 +7,13 @@ public class BuildActionSO : ActionSO
     [SerializeField] private Sprite m_CompletionSprite;
     [SerializeField] private int m_GoldCost;
     [SerializeField] private int m_WoodCost;
+    [SerializeField] private Vector3Int m_BuildingSize;
+    [SerializeField] private Vector3Int m_OriginOffset;
     public Sprite PlacementSprite => m_PlacementSprite;
     public Sprite FoundationSprite => m_FoundationSprite;
     public Sprite CompletionSprite => m_CompletionSprite;
+    public Vector3Int BuildingSize => m_BuildingSize;
+    public Vector3Int OriginOffSet => m_OriginOffset;
     public int GoldCost => m_GoldCost;
     public int WoodCost => m_WoodCost;
     public override void Execute(GameManager gameManager)
