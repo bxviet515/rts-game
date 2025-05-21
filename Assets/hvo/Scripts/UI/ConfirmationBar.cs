23299e59c7076286666abf08a7e3c0dad264f8d3
bxviet515 <vietsdk6@gmail.com>
2025-05-21 08:20:14 +0700

    Confirmation Bar

---
+++ Assets/hvo/Scripts/UI/ConfirmationBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmationBar : MonoBehaviour
+{
+    [SerializeField] private Button m_ConfirmButton;
+    [SerializeField] private Button m_CancelButton;
+    public void Show()
+    {
+
+    }
+    public void Hide()
+    {
+        
+    }
+}
